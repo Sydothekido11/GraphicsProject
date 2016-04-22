7aaa8d881d59805e18a6b43c599176e1ef260b66
Sydothekido11 <snem8901@740s-pl-109.local>
2016-04-22 12:30:47 -0600

    Added methods to the Drawing panel

--- src/graphics/view/DrawingPanel.java
+++ src/graphics/view/DrawingPanel.java
@@ -1,6 +1,50 @@
 package graphics.view;
 
-public class DrawingPanel
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import graphics.controller.GraphicsController;
+
+import javax.swing.JButton;
+import javax.swing.SpringLayout;
+import javax.swing.JPanel;
+
+public class DrawingPanel extends JPanel
 {
+	private GraphicsController baseController;
+	private SpringLayout baseLayout;
+	
+	private JButton mrButton;
+	
+	private DrawingFrame baseFrame;
+	
+	public DrawingPanel(GraphicsController baseController)
+	{
+		baseLayout = new SpringLayout();
+		this.baseController = baseController;
+		
+		mrButton = new JButton();
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
+		this.add(mrButton);
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 
 }
